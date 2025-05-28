cbc841bdba390c24a3a80da6dff609f0ee46f46f
HMMDveloper <134632686+HMMDveloper@users.noreply.github.com>
2025-05-28 21:18:38 +0500

    Implentment the pages and email_server

---
+++ pages/sent_eamil.py
@@ -0,0 +1,22 @@
+import smtplib, ssl
+
+from pages.Contact_Us import message
+
+host = "smtp.gmail.com"
+port = 465
+
+username = "malik.hamzahameed"
+password = "simple123"
+
+
+receiver = ""
+context = ssl.create_default_context()
+
+message = """
+subject Hi !
+how are you 
+bye1
+"""
+with smtplib.SMTP_SSL(host, port, context=context) as server:
+    server.login(username , password)
+    server.sendmail(username,receiver,message)
